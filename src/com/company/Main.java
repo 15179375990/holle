6de25c281752bf57e8976a79749b9d3d6fd6a85e
15179375990 <409199438@qq.com>
2019-03-12 09:36:55 +0800

    二次提交代码（新增了一个for循环）

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    System.err.println("这是一个简单的idea测试文件");
+        System.err.println("这是一个简单的idea测试文件");
+        int i = 0;
+    for (int j=0;j<10;j++){
+        System.err.println("i="+i++);
+    }
+
     }
 }
